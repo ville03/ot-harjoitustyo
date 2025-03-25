26149d5d6af60eab0871b3fb56bc50c117e8ec99
ville03 <ville.r.p.parkkinen@gmail.com>
2025-03-25 19:46:13 +0200

    viikko2 teht3

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -8,16 +8,31 @@ class TestMaksukortti(unittest.TestCase):
     def test_konstruktori_asettaa_saldon_oikein(self):
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
 
+    #def test_konstruktori_ei_aseta_saldoa_negatiiviseksi(self):
+    #    kortti = Maksukortti(-1000)
+    #    self.assertEqual(str(self.kortti), "Kortilla on rahaa 0.00 euroa")
 
     def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
         self.kortti.syo_edullisesti()
 
         self.assertEqual(self.kortti.saldo_euroina(), 7.5)
 
+    def test_syo_edullisesti_toimii_kun_kortilla_on_juuri_riittavasti_rahaa(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
     def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
         self.kortti.syo_maukkaasti()
 
         self.assertEqual(self.kortti.saldo_euroina(), 6.0)
+
+    def test_syo_maukkaasti_toimii_kun_kortilla_on_juuri_riittavasti_rahaa(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
 
     def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
         kortti = Maksukortti(200)
@@ -34,3 +49,14 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(300)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 3.0)
+
+    def test_kortille_ei_voi_ladata_negatiivista_summaa(self):
+        self.kortti.lataa_rahaa(-100)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
